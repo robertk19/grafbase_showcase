d9ea8bf50f55eeca44743bb62c71fa9f09655f21
rknie <rkni0004@student.monash.edu>
2023-10-26 16:59:11 +1100

    Auth and Sessions complete

--- lib/actions.ts
+++ lib/actions.ts
@@ -1,23 +1,39 @@
-import { getUserQuery } from "@/graphql";
+import { createUserMutation, getUserQuery } from "@/graphql";
 import { GraphQLClient } from "graphql-request";
 
 const isProduction = process.env.NODE_ENV === "production";
 const apiUrl = isProduction
   ? process.env.NEXT_PUBLICK_GRAFBASE_API_URL || ""
   : "http://127.0.0.1:4000/graphql";
-  const apiKey = isProduction ? process.env.NEXT_PUBLIC_GRAFBASE_API_URL || '' : 'letmein';
-  const serverUrl = isProduction ? process.env.NEXT_PUBLIC_SERVER_URL : "http://localhost:3000"
+const apiKey = isProduction
+  ? process.env.NEXT_PUBLIC_GRAFBASE_API_URL || ""
+  : "letmein";
+const serverUrl = isProduction
+  ? process.env.NEXT_PUBLIC_SERVER_URL
+  : "http://localhost:3000";
 
 const client = new GraphQLClient(apiUrl);
 
 const makeGraphQLRequest = async (query: string, variables = {}) => {
   try {
-    return await client.request(query, variables)
+    return await client.request(query, variables);
   } catch (error) {
-    throw (error)
+    throw error;
   }
 };
 
-export const getUser = (email: string) =>  {
-  return makeGraphQLRequest(getUserQuery, { email })
-}
+export const getUser = (email: string) => {
+  return makeGraphQLRequest(getUserQuery, { email });
+};
+
+export const createUser = (name: string, email: string, avatarUrl: string) => {
+  const varaibles = {
+    input: {
+      name,
+      email,
+      avatarUrl,
+    },
+  };
+
+  return makeGraphQLRequest(createUserMutation, { varaibles });
+};
